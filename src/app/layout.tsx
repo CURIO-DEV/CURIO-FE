4bf48dc6c65e3bed75b8410a7cc026de86c5396a
Sohyunnnn <thgus05061@naver.com>
2025-04-15 22:02:29 +0900

    rename: 파일 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Carter_One } from "next/font/google";
 import "./globals.css";
 
 const pretendard = localFont({
-  src: "styles/fonts/PretendardVariable.woff2",
+  src: "_styles/fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
