552553b20464798517531a3717f0e08f5320f006
Sohyunnnn <thgus05061@naver.com>
2025-04-17 15:40:36 +0900

    style: 스타일 수정

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -10,12 +10,12 @@ export default function Sidebar() {
     setIsLogin(true);
   };
   return (
-    <aside className="relative h-full overflow-hidden border-l-1 border-gray-100 pl-10">
+    <aside className="h-full border-l-1 border-gray-100 pl-10">
       {isLogin ? (
         //TODO: 로그인 후 사이드바
         <></>
       ) : (
-        <div className="bg-primary-50 mt-6 h-49.5 w-75 rounded-lg pt-7.5 pl-7">
+        <div className="bg-primary-50 relative mt-6 h-49.5 w-75 overflow-hidden rounded-lg pt-7.5 pl-7">
           <p className="subtitle font-semibold">
             CURIO에서
             <br />
@@ -29,7 +29,7 @@ export default function Sidebar() {
             alt="logo"
             width={257}
             height={257}
-            className="absolute top-3 left-40 opacity-3"
+            className="absolute -top-3 left-30 opacity-3"
           />
         </div>
       )}
