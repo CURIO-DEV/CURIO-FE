9b78b372cfacfe2b7e243707cd8a4432a2c2a934
Sohyunnnn <thgus05061@naver.com>
2025-04-20 20:31:58 +0900

    style: 배너 레이아웃 및 배경색 설정

--- src/app/(route)/home/page.tsx
+++ src/app/(route)/home/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import Banner from "./_components/banner";
+
 export default function Home() {
-  return <div>Home</div>;
+  const handleClick = () => {
+    // TODO: 페이지 전환
+  };
+  return (
+    <div className="mt-6 pr-10">
+      <Banner onClick={handleClick} />
+    </div>
+  );
 }
