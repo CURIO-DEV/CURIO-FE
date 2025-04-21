e13b742aae0e682366a502b78456ae6b75eeb6ed
Seugmu <anstmdal02@naver.com>
2025-04-22 00:55:06 +0900

    feat : 리뷰 반영 및 mock data 생성, 정리, 페이지 상수화화

---
+++ src/app/_mocks/dummyNews.ts
@@ -0,0 +1,30 @@
+export const dummyNews = [
+  {
+    id: 1,
+    title: "윤 대통령, 해외 순방 마치고 귀국",
+    thumbnail: "/images/newThums.png",
+    likes: 124,
+    clips: 33,
+  },
+  {
+    id: 2,
+    title: "여야, 국회 대치 장기화...해법은?",
+    thumbnail: "/images/newThums.png",
+    likes: 88,
+    clips: 56,
+  },
+  {
+    id: 3,
+    title: "서울·수도권 미세먼지 ‘나쁨’ 예보",
+    thumbnail: "/images/newThums.png",
+    likes: 203,
+    clips: 94,
+  },
+  {
+    id: 4,
+    title: "기시다, 한일정상회담 언급",
+    thumbnail: "/images/newThums.png",
+    likes: 95,
+    clips: 83,
+  },
+];
