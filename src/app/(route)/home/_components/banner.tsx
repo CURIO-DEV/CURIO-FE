f3b373c1c6d256b1eb970edae8103684f532ea72
Sohyunnnn <thgus05061@naver.com>
2025-04-21 16:30:53 +0900

    style: 스타일 수정

--- src/app/(route)/home/_components/banner.tsx
+++ src/app/(route)/home/_components/banner.tsx
@@ -9,8 +9,8 @@ interface BannerProps {
 export default function Banner({ onClick }: BannerProps) {
   return (
     <div className="flex h-100 w-full">
-      <div className="bg-primary-100 pt-22 pr-10.5 pl-7">
-        <div>
+      <div className="bg-primary-100 w-63">
+        <div className="mt-22 ml-7">
           <p className="subtitle font-semibold">
             관심있는 분야에 <br /> 핫한 뉴스를 모아보세요
           </p>
@@ -19,7 +19,7 @@ export default function Banner({ onClick }: BannerProps) {
           </Button>
         </div>
       </div>
-      <div className="bg-primary-50 w-full">시각화 컴포넌트</div>
+      <div className="bg-primary-50 flex-grow">시각화 컴포넌트</div>
     </div>
   );
 }
