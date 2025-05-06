992329edd9885d9e2f57a9facc8fff46dfbf10ef
Seugmu <anstmdal02@naver.com>
2025-05-06 19:36:11 +0900

    feat : 폴더 리스트 컴포넌트화 및 폴더 클릭 이벤트 구현현

---
+++ src/app/(route)/my-page/_components/bookmark-folder-list.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import BookmarkFolderItem from "./bookmark-folder-item";
+import { mockFolders } from "@/mocks/book-mark-folders";
+
+interface Props {
+  folders: typeof mockFolders;
+  selectedFolderId: number | null;
+  onFolderClick: (id: number) => void;
+}
+
+export default function BookmarkFolderList({
+  folders,
+  selectedFolderId,
+  onFolderClick,
+}: Props) {
+  return (
+    <div className="flex min-h-47.25 flex-col items-center rounded-lg border border-gray-200 px-3.75">
+      <div className="subTitle2 h-15 w-66.5 border-b border-gray-200 pt-5 pl-1.75">
+        북마크
+      </div>
+      {folders.length === 0 ? (
+        <p className="body1 mt-10.25 font-medium text-gray-300">
+          폴더가 없습니다
+        </p>
+      ) : (
+        <div className="my-4 flex flex-col gap-3">
+          {folders.map((folder) => (
+            <BookmarkFolderItem
+              key={folder.id}
+              name={folder.name}
+              collaborators={folder.collaborators}
+              color={folder.color}
+              onClick={() => onFolderClick(folder.id)}
+              isSelected={folder.id === selectedFolderId}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
