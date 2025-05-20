6ba952a4e3cf12857b4708e37aaed16ea473e058
Sohyunnnn <thgus05061@naver.com>
2025-05-21 01:50:17 +0900

    style: 스타일 수정

--- src/app/(route)/home/_components/word-cloud-wrapper.tsx
+++ src/app/(route)/home/_components/word-cloud-wrapper.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from "next/dynamic";
 
-// dynamic import로 클라이언트에서만 WordCloud 로드
 const WordCloudClient = dynamic(() => import("./word-cloud"), {
   ssr: false,
 });
--- src/app/(route)/home/_components/word-cloud.tsx
+++ src/app/(route)/home/_components/word-cloud.tsx
@@ -3,16 +3,19 @@
 import React from "react";
 import { Wordcloud } from "@visx/wordcloud";
 import { scaleLog } from "d3-scale";
+import seedrandom from "seedrandom";
 
 const words = [
-  { text: "React", value: 40 },
-  { text: "Next.js", value: 30 },
-  { text: "TypeScript", value: 25 },
-  { text: "Node", value: 20 },
+  { text: "대선", value: 40 },
+  { text: "윤석열", value: 30 },
+  { text: "대한민국", value: 25 },
+  { text: "정부", value: 20 },
 ];
 
 export default function WordCloudClient() {
   const fontScale = scaleLog().domain([5, 40]).range([10, 60]);
+
+  const rng = seedrandom("fixed-seed");
 
   return (
     <svg width={648} height={400}>
@@ -20,12 +23,12 @@ export default function WordCloudClient() {
         words={words}
         width={500}
         height={400}
-        font="Impact"
+        font="pretendard"
         fontSize={(d) => fontScale(d.value)}
         spiral="archimedean"
         padding={2}
         rotate={() => 0}
-        random={() => Math.random()}
+        random={() => rng()}
       >
         {(cloudWords) =>
           cloudWords.map((w, i) => (
@@ -35,6 +38,7 @@ export default function WordCloudClient() {
               transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
               fontSize={w.size}
               fontFamily={w.font}
+              fontWeight={800}
               fill="#5177ff"
             >
               {w.text}
