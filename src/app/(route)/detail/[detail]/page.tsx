9e38927b06aa824a572e022638b67401cd58b4ed
Seugmu <anstmdal02@naver.com>
2025-05-27 19:59:57 +0900

    asset: 상세페이지 좋아요 버튼 수정

--- src/app/(route)/detail/[detail]/page.tsx
+++ src/app/(route)/detail/[detail]/page.tsx
@@ -5,7 +5,13 @@ import { useArticleHeadline } from "@/hooks/use-article-headlines";
 import { useArticleSummary } from "@/hooks/use-article-summary";
 import Image from "next/image";
 import ActionBar from "../_components/action-bar";
-import { LikeFilledIcon, LikeOutlineIcon, LogoHeadIcon } from "assets";
+import {
+  LikeFilledIcon,
+  LikeOutlineIcon,
+  LogoHeadIcon,
+  DislikeFilledIcon,
+  DislikeOutlineIcon,
+} from "assets";
 
 export default function DetailPage() {
   const { detail: articleId } = useParams() as { detail: string };
@@ -67,9 +73,9 @@ export default function DetailPage() {
               <p className="caption1 mr-6 font-medium">
                 이 내용이 마음에 드시나요?
               </p>
-              <LikeFilledIcon />
-              <div className="bg-primary-200 mx-2 h-5 w-[0.5px]" />
               <LikeOutlineIcon />
+              <div className="bg-primary-200 mx-2 h-5 w-[0.5px]" />
+              <DislikeOutlineIcon />
             </div>
             <p className="body1 font-medium">
               SK㈜ 사내이사 재선임⋯올해도 리밸런싱 김건희 상설특검’ 후보자
