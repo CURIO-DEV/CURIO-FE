3b21c6cf6233090f34f6b3eeca03b6687ce5ff85
Seugmu <anstmdal02@naver.com>
2025-06-09 22:40:19 +0900

    refactor: related.ts삭제 및 리팩토링

--- src/hooks/use-related.ts
+++ src/hooks/use-related.ts
@@ -1,15 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import { getRelatedArticles, RelatedArticleResponse } from "@/apis/related";
+import { getRelatedArticles } from "@/apis/trends/trends";
+import { GetPopularArticlesResponse } from "app/_types/trend";
 
 type ExtraOpts = Partial<
-  Parameters<typeof useQuery<RelatedArticleResponse[]>>[0]
+  Parameters<typeof useQuery<GetPopularArticlesResponse[]>>[0]
 >;
 
 export function useRelatedArticles(
   articleId?: number,
   options: ExtraOpts = {},
 ) {
-  return useQuery<RelatedArticleResponse[]>({
+  return useQuery<GetPopularArticlesResponse[]>({
     queryKey: ["relatedArticles", articleId],
     queryFn: () => getRelatedArticles(articleId!),
     enabled: !!articleId,
