1387c2fac8c1a28d551aff1e1b274a79e5bc24b2
Sohyunnnn <thgus05061@naver.com>
2025-06-11 14:46:18 +0900

    style: 스타일 수정

--- src/app/_apis/user/user.ts
+++ src/app/_apis/user/user.ts
@@ -1,5 +1,9 @@
 import { END_POINTS } from "@/constants/api";
 import { apiGet, apiPatch } from "../api";
+import {
+  GetUserProfileResponse,
+  NewsletterSubscribeData,
+} from "app/_types/user";
 
 export const PatchNewsletterSubscribe = (body: NewsletterSubscribeData) => {
   return apiPatch(END_POINTS.PATCH_NEWSLETTER_SUBSCRIBE, body);
