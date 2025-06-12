28580826cf0bf294863a9aaa78720b8af5c6e984
Sohyunnnn <thgus05061@naver.com>
2025-06-12 12:53:54 +0900

    fix: 오류 해결

--- src/app/(route)/snack-news/_components/index.tsx
+++ src/app/(route)/snack-news/_components/index.tsx
@@ -4,7 +4,7 @@ import { ROUTES } from "@/constants/routes";
 import { useGetCurioGo } from "@/hooks/use-curio-go";
 import { IndexIcon } from "assets";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Index() {
