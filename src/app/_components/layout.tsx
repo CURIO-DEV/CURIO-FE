97dce810ddfdb3a9f5e6afc812a603c0028abfef
Seugmu <anstmdal02@naver.com>
2025-06-17 20:56:32 +0900

    style: 스타일 수정

--- src/app/_components/layout.tsx
+++ src/app/_components/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col justify-center">
       <Header />
       <div className="flex h-full min-w-360 px-20">
-        <main className="h-full w-full">{children}</main>
+        <main className="h-full min-h-screen w-full">{children}</main>
         {showSidebar && <Sidebar />}
       </div>
     </div>
