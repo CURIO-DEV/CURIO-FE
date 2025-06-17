7712818665b01eeaa2bfc0b9e3c9884363ac9e95
Sohyunnnn <thgus05061@naver.com>
2025-06-17 21:53:08 +0900

    feat: 디테일페이지 로딩처리

--- src/app/_components/loading-spinner.tsx
+++ src/app/_components/loading-spinner.tsx
@@ -2,7 +2,7 @@ import { SpinnerIcon } from "assets";
 
 export default function LoadingSpinner() {
   return (
-    <div className="z-10 flex h-full w-full items-center justify-center py-10">
+    <div className="z-10 flex h-screen w-full items-center justify-center py-10">
       <SpinnerIcon className="h-30 w-30 animate-spin" />
     </div>
   );
